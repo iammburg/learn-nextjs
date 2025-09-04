18e9379a5504827fcf86ba0abdbd1bcf830caf06
iammburg <ilhamprabowo009@gmail.com>
2025-09-04 19:03:14 +0700

    feat: Enhance dashboard with revenue chart and latest invoices components

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,11 @@ export default function Page() {
                 <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
                     <div className={styles.shape} />
                     <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-                        <strong>Welcome to Acme.</strong> This is the example for the{" "}
-                        <a href="https://nextjs.org/learn/" className="text-blue-500">
-                            Next.js Learn Course
-                        </a>
-                        , brought to you by Vercel.
+                        Belum ada apa-apa di sini. Lanjut ke{" "}
+                        <Link href={"/dashboard"} className="font-semibold text-blue-500 underline">
+                            Dashboard
+                        </Link>{" "}
+                        dulu aja!
                     </p>
                     <Link
                         href="/login"
